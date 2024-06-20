8b37b57db4eb5b1e8e254a684bc544b3b2fedaac
khanglk123 <lamnhatkhang131203@gmail.com>
2024-06-20 18:36:15 +0700

    Buoi06

---
+++ src/main/java/com/bookstore/validator/ValidCategoryId.java
@@ -0,0 +1,16 @@
+package com.bookstore.validator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE,FIELD})
+@Retention(RUNTIME)
+@Constraint(validatedBy = ValidCategoryIdValidator.class)
+public @interface ValidCategoryId {
+    String message() default "Danh muc khong hop le";
+    Class<?>[] groups() default {};
+    Class<? extends  Payload>[] payload() default {};
+}
